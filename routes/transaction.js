b757cdcb71248dc0583fc50316ec9a0998f7edfe
Christian Khawam <christiankhawam@outlook.com>
2019-05-26 19:57:13 -0400

    added route to display transactions

--- routes/transaction.js
+++ routes/transaction.js
@@ -5,12 +5,13 @@ const Transaction = require('../models/Transaction')
 
 router.post('/api/createTransaction', (req, res, next) => {
 
-  let { itemId, name, date, amount } = req.body
+  let { itemId, name, date, amount, userId } = req.body
   Transaction.create({
     itemId,
     name,
     amount,
-    date
+    date,
+    userId
   }).then(response => {
     console.log(response)
     res.json(response)
@@ -19,6 +20,16 @@ router.post('/api/createTransaction', (req, res, next) => {
     res.json(err)
   })
 });
+
+
+router.get('/api/transactions/:id', (req, res, next) => {
+  Transaction.find({ userId: req.params.id }).sort({ date: -1 })
+    .then(response => {
+      res.json(response)
+    }).catch(err => {
+      res.json(err)
+    })
+})
 
 
 module.exports = router;
